7727edf7e1bcba16bd1623abcaccd970d32eae26
hocine amrane <hocine.amrane@tech-instinct.com>
2025-06-02 21:19:11 +0100

    initial

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+
+const Page = () => {
+  return (
+    <>
+    <h2>Page</h2>
+    <Button>Add</Button>
+    </>
+    
+  )
+}
+
+export default Page
